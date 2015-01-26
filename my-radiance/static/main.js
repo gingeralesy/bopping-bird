37d8161620c576746b07ce19c60e0c45f53ba990
Janne Pakarinen <janne.pakarinen@pp2.inet.fi>
2015-01-26 20:23:21 +0200

    Gamepad checks

--- my-radiance/static/main.js
+++ my-radiance/static/main.js
@@ -2,11 +2,15 @@
 // Just something I can test things with.
 
 $(function() {
-  // Initialize features
-  GameState.game.init();
-  GameState.resources.path = "/static/my-radiance/"
+  if ("getGamepads" in navigator) {
+    // Initialize features
+    GameState.game.init();
+    GameState.resources.path = "/static/my-radiance/";
 
-  // Launch game
-  GameState.game.launch();
+    // Launch game
+    GameState.game.launch();
+  } else {
+    $("#main").text("Gamepads are not supported by your browser. Please update.");
+  }
 });
 
