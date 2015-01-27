89691510fed3eb4a9b73c058645e4e202fc906b1
Janne Pakarinen <janne.pakarinen@documill.com>
2015-01-27 14:35:50 +0200

    Dependency check

--- my-radiance/static/main.js
+++ my-radiance/static/main.js
@@ -1,16 +1,31 @@
 
 // Just something I can test things with.
 
+var checkDependencies = function() {
+  var errors = [];
+  if ("getGamepads" in navigator)
+  {
+    errors.push("Gamepads are not supported by your browser.");
+  }
+  return errors;
+};
+
 $(function() {
-  if ("getGamepads" in navigator) {
+  var errors = checkDependencies();
+  
+  if ($.isArray(errors) && !!errors.length) {
+    var main = $("#main");
+    $("<p>", { text : "Error!" }).appendTo(main);
+    $.each(dependenciesErrors,function(i,error) {
+      $("<p>",{ text : error }).appendTo(main);
+    });
+  } else {
     // Initialize features
     GameState.game.init();
     GameState.resources.path = "/static/my-radiance/";
 
     // Launch game
     GameState.game.launch();
-  } else {
-    $("#main").text("Gamepads are not supported by your browser. Please update.");
   }
 });
 
