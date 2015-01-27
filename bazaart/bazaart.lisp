8d522c8c39124ef9fbc0e5c94f11d726c2939f32
Janne Pakarinen <janne.pakarinen@documill.com>
2015-01-27 10:27:28 +0200

    Added index

--- bazaart/bazaart.lisp
+++ bazaart/bazaart.lisp
@@ -3,7 +3,7 @@
   (:use #:cl #:radiance #:r-clip))
 (in-package #:bazaart)
 
-(define-page roller-page #@"/" (:lquery (template "index.html"))
+(define-page roller-page #@"/" (:lquery (template "index.ctml"))
   (setf lquery:*lquery-master-document*
         (plump:strip lquery:*lquery-master-document*)))
 
